43b6b29895048683ea96d2c08059c9e77f8b02b2
u-ulim <uiissoop@gmail.com>
2025-09-01 16:27:06 +0900

    feat: test - test - tes

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col items-center justify-center h-screen"></div>
+      <div className="flex flex-col items-center justify-center h-screen">
+        test
+      </div>
     </main>
   );
 }
